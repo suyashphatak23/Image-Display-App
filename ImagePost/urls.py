51c7ab05b0c3403e1c524b96fc918fd799c34a4d
Suyash Phatak <suyashphatakbackup@gmail.com>
2021-03-12 14:00:21 +0530

    Added Main Project Files

---
+++ ImagePost/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from ImageUpload import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.blog_view, name="blog"),
+    path('<int:id>/',  views.detail_view, name="detail")
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
